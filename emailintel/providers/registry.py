44553f1c76e9e64c12946a0f4246cac3f739471b
Chandra Yadav <97451091+chandra980@users.noreply.github.com>
2026-09-24 20:40:23 +0530

    Register expanded public-source provider set

--- emailintel/providers/registry.py
+++ emailintel/providers/registry.py
@@ -1,4 +1,6 @@
+from .cert_transparency_provider import CertificateTransparencyProvider
 from .dns_provider import DNSProvider
+from .github_commit_provider import GitHubCommitProvider
 from .gravatar_provider import GravatarProvider
 from .openpgp_provider import OpenPGPProvider
 from .rdap_provider import RDAPProvider
@@ -8,6 +10,8 @@ def providers():
     return [
         DNSProvider(),
         RDAPProvider(),
+        GitHubCommitProvider(),
+        CertificateTransparencyProvider(),
         GravatarProvider(),
         OpenPGPProvider(),
     ]
